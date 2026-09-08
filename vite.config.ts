32248a72170cd8a0ad32f04a8f8500cc163d56ee
rockhubzz <compatiblewindows7@gmail.com>
2026-09-08 01:51:51 +0000

    for local deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,7 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
-  tanstackStart: {
-    server: { entry: "server" },
-  },
   nitro: {
-    preset: "static",
-    output: {
-      dir: ".output",
-      publicDir: ".output/public",
-    },
+    preset: "node-server",
   },
 });
